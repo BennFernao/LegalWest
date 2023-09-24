35c21cb9fe4dd59bd15926a2277b4013c014620f
BennFernao <bennfernao@gmail.com>
2023-09-24 17:47:59 +0100

    tes

--- redis/connect.js
+++ redis/connect.js
@@ -1,8 +1,4 @@
-const redisClient = require("redis").createClient("redis://default:ZyoX59MmP1GfdWYVelPZ1jWEGRz8McaG@redis-12292.c10.us-east-1-4.ec2.cloud.redislabs.com:12292",
-{
-    dialect:"postgres",
-        
-})
+const redisClient = require("redis").createClient("redis://default:ZyoX59MmP1GfdWYVelPZ1jWEGRz8McaG@redis-12292.c10.us-east-1-4.ec2.cloud.redislabs.com:12292")
 
 async function connectToRedis(){
     await redisClient.connect().then(()=> console.log("ligado ao redis"))
