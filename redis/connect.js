13f1de588337a2a4218dd1886f01c546d1087df9
BennFernao <bennfernao@gmail.com>
2024-02-06 14:07:09 +0100

    Versao 1.1

--- redis/connect.js
+++ redis/connect.js
@@ -1,18 +1,20 @@
 
 const  { createClient } = require('redis') ;
+/*
+const client = createClient();
+*/
 
-const client = createClient({
+
+const client = createClient(
+    {
     password: 'WCfq3liIiJu1qYdtBa2pJW6ERSCAw6Vi',
     socket: {
         host: 'redis-14598.c322.us-east-1-2.ec2.cloud.redislabs.com',
         port: 14598
     }
-});
+}
+);
 
-
-/*
-const client = createClient();
-*/
 
 async function connectToRedis(){
     await client.connect().then(()=> console.log("ligado ao redis"))
